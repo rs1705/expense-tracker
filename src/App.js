f8097047421a0480de5650468c3d9d9dd75ba82f
rs1705 <asmrahul17@gmail.com>
2022-10-22 16:28:05 +0530

    updated ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 import "./App.css";
 function App() {
   const expenses = [
@@ -26,6 +27,7 @@ function App() {
   return (
     <div className="App">
       <h1>Expense Tracker</h1>
+      <NewExpense/>
       <Expenses items={expenses} />
     </div>
   );
